fba144de9dce1fbbeeb2cac1959f5584a6ce799b
TevinStuurland <tevin94@hotmail.com>
2017-11-12 14:25:42 +0100

    added style and added speechkitt

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ if (annyang) {
 
   // Start listening.
   annyang.start();
+
+  // Tell KITT to use annyang
+  SpeechKITT.annyang();
+
+  // Define a stylesheet for KITT to use
+  SpeechKITT.setStylesheet('//cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat.css');
+
+  // Render KITT's interface
+  SpeechKITT.vroom();
 }
 
 var commands = {
