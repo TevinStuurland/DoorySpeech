81e23a474bc2f2e0ae144f104e26ca97d67ac3da
TevinStuurland <tevin94@hotmail.com>
2017-11-12 14:55:51 +0100

    added https to css link

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ if (annyang) {
   SpeechKITT.annyang();
 
   // Define a stylesheet for KITT to use
-  SpeechKITT.setStylesheet('//cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat.css');
+  SpeechKITT.setStylesheet('https://cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat.css');
 
   // Render KITT's interface
   SpeechKITT.vroom();
