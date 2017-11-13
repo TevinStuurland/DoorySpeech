b3173f761de1862405f2f0bb5a88614b9c01e0bf
TevinStuurland <tevin94@hotmail.com>
2017-11-13 12:48:55 +0100

    added Doory response to hello, changed button colors, adjusted button sizes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-var dooryButton = document.querySelector('.doory-button');
+var dooryButton = document.querySelector('.doory .button');
+var dooryText = document.querySelector('.doory .text');
 
 if (annyang) {
   // Let's define a command.
   var commands = {
-    'hello': function() { alert('Hello world!'); }, 
+    'hello': function() { dooryText.textContent += "Hello!"; }, 
     'open': function() { dooryButton.classList.add("circle"); dooryButton.classList.add("spin"); }
   };
 
@@ -23,31 +24,4 @@ if (annyang) {
   SpeechKITT.vroom();
 
   SpeechKITT.rememberStatus(120);
-}
-
-var commands = {
-  // annyang will capture anything after a splat (*) and pass it to the function.
-  // e.g. saying "Show me Batman and Robin" is the same as calling showFlickr('Batman and Robin');
-  'show me *tag': showFlickr,
-
-  // A named variable is a one word variable, that can fit anywhere in your command.
-  // e.g. saying "calculate October stats" will call calculateStats('October');
-  'calculate :month stats': calculateStats,
-
-  // By defining a part of the following command as optional, annyang will respond to both:
-  // "say hello to my little friend" as well as "say hello friend"
-  'say hello (to my little) friend': greeting
-};
-
-var showFlickr = function(tag) {
-  var url = 'http://api.flickr.com/services/rest/?tags='+tag;
-  $.getJSON(url);
-}
-
-var calculateStats = function(month) {
-  $('#stats').text('Statistics for '+month);
-}
-
-var greeting = function() {
-  $('#greeting').text('Hello!');
 }
