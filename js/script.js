e75df4fea6e64592182e1559205c3cf7fafe335c
TevinStuurland <tevin94@hotmail.com>
2017-11-13 13:25:44 +0100

    added all "spin" classes in javascript
    now the animation should run smoothly

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var dooryButton = document.querySelector('.doory .button');
 var dooryText = document.querySelector('.doory .text');
-var dooryHello = ['Hello', 'Hey'];
+var dooryHello = ['Hello!', 'Hey!'];
 
 if (annyang) {
   // Let's define a command.
@@ -10,13 +10,18 @@ if (annyang) {
             var helloThere = dooryHello[i];
             console.log(helloThere); 
         }
-        
+
         dooryText.textContent += helloThere; 
         dooryText.classList.add("dissappear");
     }, 
 
     'open': function() { 
-        dooryButton.classList.add("spin"); 
+        dooryButton.classList.add("spin", ".spin::before", ".spin::after"); 
+        dooryButton.classList.add("circle"); 
+    }, 
+
+    'help': function() { 
+        dooryButton.classList.add("spin emergency"); 
         dooryButton.classList.add("circle"); 
     }
   };
@@ -39,9 +44,28 @@ if (annyang) {
   SpeechKITT.rememberStatus(120);
 }
 
-function a(x) {   
-  function b(y) { 
-    return x + y; 
-  }
-  return b;       
+//P5 code
+
+var mic, fft;
+
+function setup() {
+   createCanvas(710,400);
+   noFill();
+
+   mic = new p5.AudioIn();
+   mic.start();
+   fft = new p5.FFT();
+   fft.setInput(mic);dwa
+}
+
+function draw() {
+   background(200);
+
+   var spectrum = fft.analyze();
+
+   beginShape();
+   for (i = 0; i<spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0) );
+   }
+   endShape();
 }
